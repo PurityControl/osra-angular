e8a4ae2b6695a9e5a28c4344f750c80cbf322a42
Craig Ferry <craig@puritycontrol.net>
2014-11-03 10:29:59 +0000

    created .gitignore for project and added subset of orphan data
    created array of subset of orphan data for index view

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-var app = angular.module('OsraApp', []);
+(function() {
+  var orphans = [
+  {
+    osra_number: 190003,
+    full_name: "Ray Smith",
+    gender: "Male",
+    status: "Active",
+    priority: "Normal",
+    mother_alive: "Yes",
+    sponsorship: "Sponsored"
+  },
+  {
+    osra_number: 160002,
+    full_name: "Mrs Peacock",
+    gender: "Female",
+    status: "Active",
+    priority: "High",
+    mother_alive: "No",
+    sponsorship: "Sponsored"
+  },
+  {
+    osra_number: 130001,
+    full_name: "Fred Bloggs",
+    gender: "Male",
+    status: "Inactive",
+    priority: "Normal",
+    mother_alive: "Yes",
+    sponsorship: "Unsponsored"
+  }
+  ];
+  var app = angular.module('OsraApp', []);
+  app.controller('OrphanController', function() {
+    this.orphans = orphans;
+  });
+})();
