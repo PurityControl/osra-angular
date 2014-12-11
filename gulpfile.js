95a045e98de179a67a4274d795c09d6e9fa81d1b
Craig Ferry <PurityControl@users.noreply.github.com>
2014-12-11 02:02:19 -0800

    created gulp linting task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
+    jshint = require('gulp-jshint'),
     connect = require('gulp-connect');
 
 gulp.task('default', function() {
   gutil.log('Starting the gulp tasks!');
+});
+
+gulp.task('lint', function() {
+  return gulp.src('builds/develop/js/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default', {verbose: true}));
 });
 
 gulp.task('server', function() {
